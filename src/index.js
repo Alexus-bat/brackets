6517110526f2a89e1de045d91d7e83d1712b459b
Alexus-bat <alexey.bond97@gmail.com>
2020-10-20 23:25:13 +0300

    fix: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = [],
+      strNew = str;
+  for (let elem of bracketsConfig) {
+    config.push(elem[0] + elem[1]);
+  }
+  for (let i = 0; i < str.length; i ++) {
+    for (let j = 0; j < config.length; j++) {
+      strNew = strNew.replace(config[j], '');      
+    }
+    if (strNew.length == 0) return true;   
+  }
+  return false;  
 }
